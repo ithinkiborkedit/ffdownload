4ce16e6e3e659586475a2a72b2b3f9e0f17223e6
ithinkiborkedit <miles.warburton@gmail.com>
2024-06-12 14:00:05 +0200

    refactoring

---
+++ src/downloadclient/downloadclient.go
@@ -0,0 +1,30 @@
+package downloadclient
+
+import (
+	"net/http"
+)
+
+type FfDownloadClient struct {
+	Client *http.Client
+	Url    string
+}
+
+func New(client *http.Client, url string) *FfDownloadClient {
+	return &FfDownloadClient{
+		Client: client,
+		Url:    url,
+	}
+}
+
+func (ffc *FfDownloadClient) Do(req *http.Request) (*http.Response, error) {
+	return ffc.Client.Do(req)
+}
+
+func (ffc *FfDownloadClient) ChunkGet(url string, headervalue string) (*http.Response, error) {
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Add("RANGE", headervalue)
+	return ffc.Do(req)
+}

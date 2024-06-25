b0e43eee5c738068c40061097358e0dde0f4a85c
ithinkiborkedit <miles.warburton@gmail.com>
2024-06-25 13:29:43 +0200

    refactor

---
+++ src/log/logger.go
@@ -0,0 +1,36 @@
+package logger
+
+import (
+	"os"
+
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+var Logger *zap.Logger
+
+func CreateLogger() *zap.Logger {
+	encoderCfg := zap.NewProductionEncoderConfig()
+	encoderCfg.TimeKey = "timestamp"
+	encoderCfg.EncodeTime = zapcore.ISO8601TimeEncoder
+	encoderCfg.EncodeLevel = zapcore.CapitalColorLevelEncoder
+
+	config := zap.Config{
+		Level:             zap.NewAtomicLevelAt(zap.ErrorLevel),
+		DisableStacktrace: true,
+		Sampling:          nil,
+		Encoding:          "json",
+		EncoderConfig:     encoderCfg,
+		OutputPaths: []string{
+			"stderr",
+		},
+		ErrorOutputPaths: []string{
+			"stderr",
+		},
+		InitialFields: map[string]interface{}{
+			"pid": os.Getpid(),
+		},
+	}
+	return zap.Must(config.Build())
+
+}
